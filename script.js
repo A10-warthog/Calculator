1b0b8f07c494c5a4ef29a6426270df422d5b2e4c
A10-warthog <behemoth11111111@gmail.com>
2022-12-17 23:58:23 +0530

    Add remaining operator case

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function calculatorMain() {
     function operator(a, sign, b) {
         switch(sign) {
             case '+':
-                return a + b; 
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b; 
         }
     }
 }
