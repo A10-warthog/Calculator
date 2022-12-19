45be64db426b4c06931586a04a067b9b51233be6
A10-warthog <behemoth11111111@gmail.com>
2022-12-19 23:29:22 +0530

    Add variable to store non alpha numeric characters

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function calculatorMain() {    
     const numButton = document.querySelector(".button__number").children;
+    const signButton = document.querySelector(".button__sign").children;
     function operator(a, sign, b) {
         switch(sign) {
             case '+':
