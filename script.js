495428f80bfb8b7d5bdd633c31dabbf1bfcd945e
A10-warthog <behemoth11111111@gmail.com>
2022-12-23 23:30:28 +0530

    Add variable to store last operand

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculatorMain() {
     let numFirst = null;
-        
+    let numLast = null;    
     const numButton = document.querySelector(".button__number").children;
     const signButton = document.querySelector(".button__sign").children;
     function operator(a, sign, b) {
