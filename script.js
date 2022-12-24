30b78eec02f3073dd5de34a307279af6d035cc33
A10-warthog <behemoth11111111@gmail.com>
2022-12-24 23:56:37 +0530

    Add variable to store display value

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function calculatorMain() {
     let numFirst = '';
     let numLast = '';
+    let strNumber = '';
     let operate = null;
-    let lastOperate = null;    
+    let lastOperate = null;
+        
     const numButton = document.querySelector(".button__number").children;
     const signButton = document.querySelector(".button__sign").children;
     function operator(a, sign, b) {
