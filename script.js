881457b8643ecda637826f07c1ac034f9384e181
A10-warthog <behemoth11111111@gmail.com>
2022-12-31 23:49:24 +0530

    Remove calculator function

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-function calculatorMain() {
-    let numFirst = '';
-    let numLast = '';
-    let strNumber = '';
-    let operator = null;
-    let lastOperator = null;
-      
-    const numButton = document.querySelector(".button__number").children;
-    const signButton = document.querySelector(".button__sign").children;
+
+let numFirst = '';
+let numLast = '';
+let strNumber = '';
+let operator = null;
+let lastOperator = null;
+  
+const numButton = document.querySelector(".button__number").children;
+const signButton = document.querySelector(".button__sign").children;
+
+function clearBtn() {
+     numFirst = '';
+     numLast = '';
+     strNumber = '';
+     operator = null;
+     lastOperator = null;
+}
+
+function addOperator(operateSign) {
+    if (operateSign === '%') 
+        operateSign = '+';
+    strNumber = '';
     
-    function clearBtn() {
-         numFirst = '';
-         numLast = '';
-         strNumber = '';
-         operator = null;
-         lastOperator = null;
-    }
+    if (operator !== null && numLast !== '')
+        lastOperator = operateSign;
+    else 
+        operator = operateSign;
+}
 
-    function addOperator(operateSign) {
-        if (operateSign === '%') 
-            operateSign = '+';
-        strNumber = '';
-        
-        if (operator !== null && numLast !== '')
-            lastOperator = operateSign;
-        else 
-            operator = operateSign;
-    }
-
-    function operateOnNumber(a, sign, b) {
-        switch(sign) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                return a / b; 
-        }
+function operateOnNumber(a, sign, b) {
+    switch(sign) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b; 
     }
 }
